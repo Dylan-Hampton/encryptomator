c3de5508864501bf127b7d73675fab98899ac51e
btnguyen <btnguyen@pyrite-n3.cs.iastate.edu>
2022-04-18 15:10:07 -0500

    added user input for i/o buffer sizes

--- encrypt-driver.c
+++ encrypt-driver.c
@@ -16,6 +16,13 @@ int main(int argc, char *argv[]) {
   }
   //printf("%s\n%s\n%s\n", argv[1], argv[2], argv[3]);
 	init(argv[1], argv[2], argv[3]); 
+    printf("Size of input buffer N:\n");
+    int n = -1;
+    int m = -1;
+    scanf("%d", &n);
+    printf("Size of input buffer M:\n");
+    scanf("%d", &m);
+    printf("N:%d\nM:%d\n", n, m);
 	char c;
 	while ((c = read_input()) != EOF) { 
 		count_input(c); 
